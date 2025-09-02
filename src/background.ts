94d268c5e7e45f8917ccc80ef4e9519948c8c4de
Tony George <tg764562@gmail.com>
2025-09-02 23:08:46 +0300

    refactor: reuse tab handler

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,11 @@
 /// <reference types="chrome"/>
 chrome.tabs.onCreated.addListener((tab: chrome.tabs.Tab) => {
-  console.log("opened", tab.url);
+  onNavigation(tab);
 });
 chrome.tabs.onUpdated.addListener((_id, _, tab: chrome.tabs.Tab) => {
-  console.log("navigated", tab.url);
+  onNavigation(tab);
 });
+
+const onNavigation = (tab: chrome.tabs.Tab) => {
+  console.log('current tab:', tab.url);
+}
